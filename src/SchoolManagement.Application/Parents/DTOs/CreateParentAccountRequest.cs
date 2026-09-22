ec9c19429ecc9b511cea3fce309dcbeb13ca9517
Mohamed Jaseeth SOFTWARE ALHAZM <jaseeth@alhazm.com>
2026-09-22 17:23:43 +0300

    commit

---
+++ src/SchoolManagement.Application/Parents/DTOs/CreateParentAccountRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagement.Application.Parents.DTOs;
+
+public class CreateParentAccountRequest
+{
+    public int ParentGuardianId { get; set; }
+
+    public string Password { get; set; } = string.Empty;
+
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
